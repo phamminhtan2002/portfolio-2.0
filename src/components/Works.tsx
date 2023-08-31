c999e79bfe7f574beaeb843da696706ae67af53c
phamminhtan2002 <tanphamminh2002@gmail.com>
2023-08-31 13:29:06 -0700

    improve styles

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -1,121 +1,115 @@
-import Tilt from 'react-parallax-tilt';
-import { motion } from 'framer-motion';
-import { styles } from '../styles';
-import { github } from '../assets';
-import { SectionWrapper } from '../hoc';
-import { projects } from '../constants';
-import { fadeIn, textVariant } from '../utils/motion';
+import { motion } from 'framer-motion'
+import Tilt from 'react-parallax-tilt'
+import { github } from '../assets'
+import { projects } from '../constants'
+import { SectionWrapper } from '../hoc'
+import { styles } from '../styles'
+import { fadeIn, textVariant } from '../utils/motion'
 
 type Props = {
-	index: number;
-	name: string;
-	description: string;
-	tags: { name: string; color: string }[];
-	image: string;
-	source_code_link: string;
-};
+  index: number
+  name: string
+  description: string
+  tags: { name: string; color: string }[]
+  image: string
+  source_code_link: string
+}
 
 const style = {
-	content__con: `mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]`,
-	link__con: `absolute inset-0 flex justify-end m-3 card-img_hover`,
-	link: `black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer`,
-};
+  content__con: `mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]`,
+  link__con: `absolute inset-0 flex justify-end m-3 card-img_hover`,
+  link: `black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer`,
+}
 
 const ProjectCard = ({
-	index,
-	name,
-	description,
-	tags,
-	image,
-	source_code_link,
+  index,
+  name,
+  description,
+  tags,
+  image,
+  source_code_link,
 }: Props) => {
-	return (
-		<motion.div
-			variants={fadeIn({
-				direction: 'up',
-				type: 'spring',
-				delay: index * 0.5, //comming up 1 by 1
-				duration: 0.75,
-			})}>
-			<Tilt
-				tiltMaxAngleX={34}
-				tiltMaxAngleY={34}
-				scale={1.04}
-				transitionSpeed={450}
-				className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full">
-				<div className="relative w-full h-[230px]">
-					<img
-						src={image}
-						alt={name}
-						className="w-full h-full object-cover rounded-2xl"
-					/>
+  return (
+    <motion.div
+      variants={fadeIn({
+        direction: 'up',
+        type: 'spring',
+        delay: index * 0.5, //comming up 1 by 1
+        duration: 0.75,
+      })}>
+      <Tilt
+        tiltMaxAngleX={34}
+        tiltMaxAngleY={34}
+        scale={1.04}
+        transitionSpeed={450}
+        className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'>
+        <div className='relative w-full h-[230px]'>
+          <img
+            src={image}
+            alt={name}
+            className='w-full h-full object-cover rounded-2xl'
+          />
 
-					<div className={style.link__con}>
-						<div
-							onClick={() => window.open(source_code_link, '_blank')}
-							className={style.link}>
-							<img
-								src={github}
-								alt="github"
-								className="w-1/2 h-1/2 object-contain"
-							/>
-						</div>
-					</div>
-				</div>
+          <div className={style.link__con}>
+            <div
+              onClick={() => window.open(source_code_link, '_blank')}
+              className={style.link}>
+              <img
+                src={github}
+                alt='github'
+                className='w-1/2 h-1/2 object-contain'
+              />
+            </div>
+          </div>
+        </div>
 
-				<div className="mt-5">
-					<h3 className="text-white font-bold text-[24px]">{name}</h3>
-					<p className="mt-2 text-secondary text-[14px]">{description}</p>
-				</div>
+        <div className='mt-5'>
+          <h3 className='text-white font-bold text-[24px]'>{name}</h3>
+          <p className='mt-2 text-secondary text-[14px]'>{description}</p>
+        </div>
 
-				<div className="mt-4 flex flex-wrap gap-2">
-					{tags.map((tag) => (
-						<p
-							key={tag.name}
-							className={`text-[14px] ${tag.color}`}>
-							#{tag.name}
-						</p>
-					))}
-				</div>
-			</Tilt>
-		</motion.div>
-	);
-};
+        <div className='mt-4 flex flex-wrap gap-2'>
+          {tags.map((tag) => (
+            <p key={tag.name} className={`text-[14px] ${tag.color}`}>
+              #{tag.name}
+            </p>
+          ))}
+        </div>
+      </Tilt>
+    </motion.div>
+  )
+}
 
 const Works = () => {
-	return (
-		<>
-			<motion.div variants={textVariant()}>
-				<p className={styles.sectionSubText}>My work</p>
-				<h2 className={styles.sectionHeadText}>Projects.</h2>
-			</motion.div>
-			<div className="w-full flex">
-				<motion.p
-					variants={fadeIn({
-						direction: '',
-						type: '',
-						delay: 0.1,
-						duration: 1,
-					})}
-					className={style.content__con}>
-					These projects showcase my skills and experiences through real-world
-					examples of my work. Each project is briefly described with a link to
-					the code repository and live demos in it. It reflects my ability to
-					solve complex problems, work with different technologies, and manage
-					projects effiently.
-				</motion.p>
-			</div>
-			<div className="mt-20 flex flex-wrap gap-7">
-				{projects.map((project, index) => (
-					<ProjectCard
-						key={`project-${index}`}
-						index={index}
-						{...project}
-					/>
-				))}
-			</div>
-		</>
-	);
-};
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>My work</p>
+        <h2 className={styles.sectionHeadText}>Projects.</h2>
+      </motion.div>
+      <div className='w-full flex'>
+        <motion.p
+          variants={fadeIn({
+            direction: '',
+            type: '',
+            delay: 0.1,
+            duration: 1,
+          })}
+          className={style.content__con}>
+          These projects showcase my skills and experiences through real-world
+          examples of my work. Each project is briefly described with a link to
+          the code repository and live demos in it. It reflects my ability to
+          solve complex problems, work with different technologies, and manage
+          projects effiently.
+        </motion.p>
+      </div>
+      <div className='mt-20 flex flex-wrap gap-7'>
+        {projects.map((project, index) => (
+          <ProjectCard key={`project-${index}`} index={index} {...project} />
+        ))}
+      </div>
+    </>
+  )
+}
 
-export default SectionWrapper({ Component: Works, idName: '' });
+export default SectionWrapper({ Component: Works, idName: 'projects' })
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -26,265 +26,270 @@ import {
 } from '../assets';
 
 const navLinks = [
-	{
-		id: 'about',
-		title: 'About',
-	},
-	{
-		id: 'work',
-		title: 'Work',
-	},
-	{
-		id: 'contact',
-		title: 'Contact',
-	},
-];
+  {
+    id: 'about',
+    title: 'About',
+  },
+  {
+    id: 'work',
+    title: 'Work',
+  },
+  {
+    id: 'projects',
+    title: 'Projects',
+  },
+  {
+    id: 'contact',
+    title: 'Contact',
+  },
+]
 
 const jobs = [
-	'Web Development',
-	'Web Design',
-	'Data Engineering',
-	'Cloud Engineering',
-	'React.js',
-	'MySQL',
-	'Python',
-	'Docker',
-	'Kubernetes',
-	'Amazon Web Services',
-	'CI/CD',
-	'Jenkins',
-	'Node.js',
-	'Windows Server',
-	'DNS',
-	'VPN',
-	'DHCP',
-	'Next.js',
-	'WordPress',
-	'Microservices',
-	'HTML',
-	'CSS',
-	'TailwindCSS',
-	'TypeScript',
-	'JavaScript',
-	'Git',
-	'Linux',
-	'Microservices',
-];
+  'Web Development',
+  'Web Design',
+  'Data Engineering',
+  'Cloud Engineering',
+  'React.js',
+  'MySQL',
+  'Python',
+  'Docker',
+  'Kubernetes',
+  'Amazon Web Services',
+  'CI/CD',
+  'Jenkins',
+  'Node.js',
+  'Windows Server',
+  'DNS',
+  'VPN',
+  'DHCP',
+  'Next.js',
+  'WordPress',
+  'Microservices',
+  'HTML',
+  'CSS',
+  'TailwindCSS',
+  'TypeScript',
+  'JavaScript',
+  'Git',
+  'Linux',
+  'Microservices',
+]
 
 const services = [
-	{
-		title: 'Web Developer',
-		icon: web,
-	},
-	{
-		title: 'React.js Developer',
-		icon: mobile,
-	},
-	{
-		title: 'Backend Developer',
-		icon: backend,
-	},
-	{
-		title: 'Microservices Engineer',
-		icon: creator,
-	},
-];
+  {
+    title: 'Web Developer',
+    icon: web,
+  },
+  {
+    title: 'React.js Developer',
+    icon: mobile,
+  },
+  {
+    title: 'Backend Developer',
+    icon: backend,
+  },
+  {
+    title: 'Microservices Engineer',
+    icon: creator,
+  },
+]
 
 const technologies = [
-	{
-		name: 'HTML 5',
-		icon: html,
-	},
-	{
-		name: 'CSS 3',
-		icon: css,
-	},
-	{
-		name: 'JavaScript',
-		icon: javascript,
-	},
-	{
-		name: 'TypeScript',
-		icon: typescript,
-	},
-	{
-		name: 'nextjs',
-		icon: nextjs,
-	},
-	{
-		name: 'React JS',
-		icon: reactjs,
-	},
-	{
-		name: 'Redux Toolkit',
-		icon: redux,
-	},
-	{
-		name: 'Tailwind CSS',
-		icon: tailwind,
-	},
-	{
-		name: 'Node JS',
-		icon: nodejs,
-	},
-	{
-		name: 'MongoDB',
-		icon: mongodb,
-	},
-	{
-		name: 'git',
-		icon: git,
-	},
-	{
-		name: 'Kubernetes',
-		icon: kubernetes,
-	},
-	{
-		name: 'docker',
-		icon: docker,
-	},
-];
+  {
+    name: 'HTML 5',
+    icon: html,
+  },
+  {
+    name: 'CSS 3',
+    icon: css,
+  },
+  {
+    name: 'JavaScript',
+    icon: javascript,
+  },
+  {
+    name: 'TypeScript',
+    icon: typescript,
+  },
+  {
+    name: 'nextjs',
+    icon: nextjs,
+  },
+  {
+    name: 'React JS',
+    icon: reactjs,
+  },
+  {
+    name: 'Redux Toolkit',
+    icon: redux,
+  },
+  {
+    name: 'Tailwind CSS',
+    icon: tailwind,
+  },
+  {
+    name: 'Node JS',
+    icon: nodejs,
+  },
+  {
+    name: 'MongoDB',
+    icon: mongodb,
+  },
+  {
+    name: 'git',
+    icon: git,
+  },
+  {
+    name: 'Kubernetes',
+    icon: kubernetes,
+  },
+  {
+    name: 'docker',
+    icon: docker,
+  },
+]
 
 const experiences = [
-	{
-		title: 'Computer Information Technology Student',
-		company_name: 'British Columbia Institute of Technology',
-		icon: bcit,
-		iconBg: '#383E56',
-		date: 'Sep 2020 - Dec 2023',
-		points: [
-			'Homework Tracker App: Constructed a web application using JavaScript, Python, Flask, NPM, Node.js, and SQLite. Designed and implemented a user-friendly interface for BCIT undergrads to track their assignments and test deadlines. Successfully deployed the app on Heroku and received positive feedback from users.',
-			'Microservices Architecture: Built a microservices architecture using Docker and Kubernetes for application API communication, with Jenkins CI/CD pipeline for automated deployment.',
-			'Windows Server Virtual Machine: Developed and maintained a Windows Server 2022 virtual machine using VMware Workstation Pro. Set up Active Directory Domain Services, users, file servers, group policies, iSCSI, DNS, and DHCP.',
-		],
-	},
-	{
-		title: 'WordPress Developer',
-		company_name: 'VNT Trading',
-		icon: vnt,
-		iconBg: '#E6DEDD',
-		date: 'June 2021 - Sep 2022',
-		points: [
-			'Constructed a full-stack e-commerce website using WordPress and WooCommerce.',
-			'Implemented responsive design and ensured cross-browser compatibility.',
-			'Organized and uploaded product data to the web.',
-			'Implemented designs that included custom post-types & taxonomies, as well as social media integration.',
-		],
-	},
-	{
-		title: 'Full-stack Developer',
-		company_name: 'BC Centre for Aquatic Health Sciences Society',
-		icon: bccahs,
-		iconBg: '#383E56',
-		date: 'Sep 2022 - Dec 2022',
-		points: [
-			'Worked as a full stack developer through BCIT’s Industry Sponsored Project.',
-			'Designed and developed a statistical analysis web application using Python Flask, R, and MySQL.',
-			'Deployed and established secured connections between front end application and MySQL database on Arbutus Cloud.',
-			'Enabled interactive and user-friendly visualization on large-scale data sets.',
-			'Tested the website prior to live deployment by performing unit testing, integration testing, and E2E testing.',
-			'Followed industry best practices, including Agile methodology, DevOps, and code reviews.',
-		],
-	},
-];
+  {
+    title: 'Computer Information Technology Student',
+    company_name: 'British Columbia Institute of Technology',
+    icon: bcit,
+    iconBg: '#383E56',
+    date: 'Sep 2020 - Dec 2023',
+    points: [
+      'Homework Tracker App: Constructed a web application using JavaScript, Python, Flask, NPM, Node.js, and SQLite. Designed and implemented a user-friendly interface for BCIT undergrads to track their assignments and test deadlines. Successfully deployed the app on Heroku and received positive feedback from users.',
+      'Microservices Architecture: Built a microservices architecture using Docker and Kubernetes for application API communication, with Jenkins CI/CD pipeline for automated deployment.',
+      'Windows Server Virtual Machine: Developed and maintained a Windows Server 2022 virtual machine using VMware Workstation Pro. Set up Active Directory Domain Services, users, file servers, group policies, iSCSI, DNS, and DHCP.',
+    ],
+  },
+  {
+    title: 'WordPress Developer',
+    company_name: 'VNT Trading',
+    icon: vnt,
+    iconBg: '#E6DEDD',
+    date: 'June 2021 - Sep 2022',
+    points: [
+      'Constructed a full-stack e-commerce website using WordPress and WooCommerce.',
+      'Implemented responsive design and ensured cross-browser compatibility.',
+      'Organized and uploaded product data to the web.',
+      'Implemented designs that included custom post-types & taxonomies, as well as social media integration.',
+    ],
+  },
+  {
+    title: 'Full-stack Developer',
+    company_name: 'BC Centre for Aquatic Health Sciences Society',
+    icon: bccahs,
+    iconBg: '#383E56',
+    date: 'Sep 2022 - Dec 2022',
+    points: [
+      'Worked as a full stack developer through BCIT’s Industry Sponsored Project.',
+      'Designed and developed a statistical analysis web application using Python Flask, R, and MySQL.',
+      'Deployed and established secured connections between front end application and MySQL database on Arbutus Cloud.',
+      'Enabled interactive and user-friendly visualization on large-scale data sets.',
+      'Tested the website prior to live deployment by performing unit testing, integration testing, and E2E testing.',
+      'Followed industry best practices, including Agile methodology, DevOps, and code reviews.',
+    ],
+  },
+]
 
 const testimonials = [
-	{
-		testimonials: [
-			'Completed a database engineering program taught by industry-recognized experts at Meta.',
-			'Demonstrated my proficiency in different aspects of database engineering through a series of five projects that involved database normalization, stored procedures, Python applications, data modeling, and MySQL database solutions.',
-		],
-		name: 'Coursera',
-		date: 'May 18, 2023',
-		image: meta,
-	},
-];
+  {
+    testimonials: [
+      'Completed a database engineering program taught by industry-recognized experts at Meta.',
+      'Demonstrated my proficiency in different aspects of database engineering through a series of five projects that involved database normalization, stored procedures, Python applications, data modeling, and MySQL database solutions.',
+    ],
+    name: 'Coursera',
+    date: 'May 18, 2023',
+    image: meta,
+  },
+]
 
 const projects = [
-	{
-		name: 'Finance Tracker',
-		description:
-			'MERN stack web application that allows users to add, delete income and expenses, with an overview graph display. It is a convenient and efficient solution for managing net-income.',
-		tags: [
-			{
-				name: 'mongodb',
-				color: 'green-text-gradient',
-			},
-			{
-				name: 'express',
-				color: 'pink-text-gradient',
-			},
-			{
-				name: 'react',
-				color: 'blue-text-gradient',
-			},
-			{
-				name: 'nodejs',
-				color: 'green-yellow-text-gradient',
-			},
-		],
-		image: financetracker,
-		source_code_link: 'https://github.com/phamminhtan2002/finance-tracker',
-	},
-	{
-		name: 'Amazon Storefront',
-		description:
-			'Full stack e-commerce website that replicates Amazon.ca, including user authentication, order history, and secure payment processing with debit/credit cards.',
-		tags: [
-			{
-				name: 'react',
-				color: 'blue-text-gradient',
-			},
-			{
-				name: 'firebase',
-				color: 'yellow-text-gradient',
-			},
-			{
-				name: 'tailwindcss',
-				color: 'pink-text-gradient',
-			},
-			{
-				name: 'typescript',
-				color: 'orange-text-gradient',
-			},
-		],
-		image: amazon,
-		source_code_link: 'https://github.com/phamminhtan2002/amazon-fullstack',
-	},
-	{
-		name: 'Video Streaming System',
-		description:
-			'A complex microservices web application that ultilizes Kubernetes to deploy on Amazon EKS. It has Nextjs as the frontend and Express with MySQL as the backend.',
-		tags: [
-			{
-				name: 'nextjs',
-				color: 'black-text-gradient',
-			},
-			{
-				name: 'mysql',
-				color: 'green-text-gradient',
-			},
-			{
-				name: 'docker',
-				color: 'blue-text-gradient',
-			},
-			{
-				name: 'kubernetes',
-				color: 'pink-text-gradient',
-			},
-		],
-		image: videostream,
-		source_code_link:
-			'https://github.com/phamminhtan2002/Video-Streaming-System/tree/kubernetes',
-	},
-];
+  {
+    name: 'Finance Tracker',
+    description:
+      'MERN stack web application that allows users to add, delete income and expenses, with an overview graph display. It is a convenient and efficient solution for managing net-income.',
+    tags: [
+      {
+        name: 'mongodb',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'express',
+        color: 'pink-text-gradient',
+      },
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'nodejs',
+        color: 'green-yellow-text-gradient',
+      },
+    ],
+    image: financetracker,
+    source_code_link: 'https://github.com/phamminhtan2002/finance-tracker',
+  },
+  {
+    name: 'Amazon Storefront',
+    description:
+      'Full stack e-commerce website that replicates Amazon.ca, including user authentication, order history, and secure payment processing with debit/credit cards.',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'firebase',
+        color: 'yellow-text-gradient',
+      },
+      {
+        name: 'tailwindcss',
+        color: 'pink-text-gradient',
+      },
+      {
+        name: 'typescript',
+        color: 'orange-text-gradient',
+      },
+    ],
+    image: amazon,
+    source_code_link: 'https://github.com/phamminhtan2002/amazon-fullstack',
+  },
+  {
+    name: 'Video Streaming System',
+    description:
+      'A complex microservices web application that ultilizes Kubernetes to deploy on Amazon EKS. It has Nextjs as the frontend and Express with MySQL as the backend.',
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'black-text-gradient',
+      },
+      {
+        name: 'mysql',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'docker',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'kubernetes',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: videostream,
+    source_code_link:
+      'https://github.com/phamminhtan2002/Video-Streaming-System/tree/kubernetes',
+  },
+]
 
 export {
-	experiences,
-	jobs,
-	navLinks,
-	projects,
-	services,
-	technologies,
-	testimonials,
-};
+  experiences,
+  jobs,
+  navLinks,
+  projects,
+  services,
+  technologies,
+  testimonials,
+}
+
