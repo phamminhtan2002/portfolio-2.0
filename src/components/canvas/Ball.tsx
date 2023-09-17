1071ed3eeb9431d94708168d517276cc6da6aaae
garenpham <tanphamminh2002@gmail.com>
2023-09-16 23:55:45 -0700

    change work experience

--- src/components/canvas/Ball.tsx
+++ src/components/canvas/Ball.tsx
@@ -1,63 +1,54 @@
-import { Suspense } from 'react';
-import { Canvas } from '@react-three/fiber';
 import {
-	OrbitControls,
-	Decal,
-	Float,
-	Preload,
-	useTexture,
-} from '@react-three/drei';
-import CanvasLoader from '../Loader';
+  Decal,
+  Float,
+  OrbitControls,
+  Preload,
+  useTexture,
+} from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import { Suspense } from 'react'
+import CanvasLoader from '../Loader'
 
 type Props = {
-	icon: string;
-};
+  icon: string
+}
 
 const Ball = ({ icon }: Props) => {
-	const [decal] = useTexture([icon]);
+  const [decal] = useTexture([icon])
 
-	return (
-		<Float
-			speed={1.75}
-			rotationIntensity={1}
-			floatIntensity={2}>
-			<ambientLight intensity={0.25} />
-			<directionalLight position={[0, 0, 0.05]} />
-			<mesh
-				castShadow
-				receiveShadow
-				scale={2.75}>
-				<icosahedronGeometry args={[1, 1]} />
-				<meshStandardMaterial
-					color="#fff8eb"
-					polygonOffset
-					polygonOffsetFactor={-5}
-					flatShading
-				/>
-				<Decal
-					position={[0, 0, 1]}
-					rotation={[2 * Math.PI, 0, 6.25]} //Mirror the icon properly
-					flatShading
-					map={decal}
-				/>
-			</mesh>
-		</Float>
-	);
-};
+  return (
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={0.25} />
+      <directionalLight position={[0, 0, 0.05]} />
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial
+          color='#fff8eb'
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        <Decal
+          position={[0, 0, 1]}
+          rotation={[2 * Math.PI, 0, 6.25]} //Mirror the icon properly
+          map={decal}
+        />
+      </mesh>
+    </Float>
+  )
+}
 
 const BallCanvas = ({ icon }: Props) => {
-	return (
-		<Canvas
-			frameloop="demand"
-			gl={{ preserveDrawingBuffer: true }}>
-			<Suspense fallback={<CanvasLoader />}>
-				<OrbitControls enableZoom={false} />
-				<Ball icon={icon} />
-			</Suspense>
+  return (
+    <Canvas frameloop='demand' gl={{ preserveDrawingBuffer: true }}>
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false} />
+        <Ball icon={icon} />
+      </Suspense>
 
-			<Preload all />
-		</Canvas>
-	);
-};
+      <Preload all />
+    </Canvas>
+  )
+}
 
-export default BallCanvas;
+export default BallCanvas
